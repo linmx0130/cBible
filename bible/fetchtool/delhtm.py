b9524bc977b2a61b061f3a298840945a85cc2d6f
Sweetdumplings <linmx0130@163.com>
2014-04-20 14:48:09 +0800

    基本功能完成，改进抓取工具，添加罗马书

--- bible/fetchtool/delhtm.py
+++ bible/fetchtool/delhtm.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 n=int(input("Total:"))
 for i in range(1,n+1):
     f1=open("%d.txt"%(i),"r")
--- cBible.py
+++ cBible.py
@@ -4,21 +4,45 @@
 import wx
 
 class MainWindow ( wx.Frame ) :
-    index=[]
-    def loadBible(self,event,name):
-        print (name)
+    index={}
+    loadedname=""
+    def loadinfact(self,name,chap):
+        self.loadedname=name
+        self.contents.Clear()
+        f=open(r"bible/"+name+r"/%d.txt"%chap,"r")
+        txt=""
+        for t in f:
+            txt+=t
+        f.close()
+        self.nowChap.SetValue(str(chap))
+        self.contents.SetValue(txt)
 
+    def loadBible(self,event,name):
+        self.loadinfact(name,1)
+        text="正在阅读《"+self.index[name][0]+"》，共"+str(self.index[name][1])+"章"
+        self.StatusBar.PushStatusText(text)
     def getIndex(self):
         retmenu=wx.Menu()
         index_fh=open(r"bible/index","r")
         for buf in index_fh:
-            print(buf)
-            (dirname,title)=buf.split("|")
+            (dirname,filetotal,title)=buf.split("|")
             title=title.strip('\n')
             btn=retmenu.Append(wx.NewId(),title )
             self.Bind(wx.EVT_MENU, lambda evt, tmp=dirname:self.loadBible(evt,tmp), btn)
-            self.index.append( (dirname,title) )
+            self.index[dirname]=[title,int(filetotal)]
+        index_fh.close()
         return retmenu
+    def OnToLeftBtn(self,event):
+        nowchap=int(self.nowChap.GetValue())
+        if (nowchap -1  >0 ):
+            nowchap = nowchap -1
+            self.loadinfact(self.loadedname,nowchap)
+
+    def OnToRightBtn(self,event):
+        nowchap=int(self.nowChap.GetValue())
+        if (nowchap + 1 <= self.index[self.loadedname][1]):
+            nowchap = nowchap +1
+            self.loadinfact(self.loadedname,nowchap)
 
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title)
@@ -43,15 +67,18 @@ class MainWindow ( wx.Frame ) :
         #build the chapter choicer
         self.chapch=wx.BoxSizer()
         self.toLeftBtn=wx.Button(self.bkg,label="<-")
+        self.Bind(wx.EVT_BUTTON,self.OnToLeftBtn,self.toLeftBtn)
         self.toRightBtn=wx.Button(self.bkg,label="->")
+        self.Bind(wx.EVT_BUTTON,self.OnToRightBtn,self.toRightBtn)
         self.nowChap=wx.TextCtrl(self.bkg)
         self.chapch.Add(self.toLeftBtn,proportion=0,flag=wx.LEFT ,border=5)
         self.chapch.Add(self.nowChap,proportion=1,flag=wx.EXPAND)
         self.chapch.Add(self.toRightBtn,proportion=0,flag=wx.LEFT ,border=5)
         self.box.Add(self.chapch,proportion=0,flag=wx.EXPAND|wx.ALL,border=5)
         #build the contents container
-        contents=wx.TextCtrl(self.bkg,style=wx.TE_MULTILINE|wx.HSCROLL)
-        self.box.Add(contents,proportion=1,flag=wx.EXPAND|wx.BOTTOM|wx.LEFT|wx.RIGHT)
+        self.contents=wx.TextCtrl(self.bkg,style=wx.TE_MULTILINE|wx.HSCROLL)
+        self.contents.SetEditable(False)
+        self.box.Add(self.contents,proportion=1,flag=wx.EXPAND|wx.BOTTOM|wx.LEFT|wx.RIGHT)
         
         #the last part
         self.bkg.SetSizer(self.box)
